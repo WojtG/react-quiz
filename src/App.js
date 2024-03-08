8e85171d2076470e6994f7167267082183b8b3fb
WojtG <wojtekgg431@gmail.com>
2024-03-08 21:14:12 +0100

    create fake api and fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceive":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "error":
+      return { ...state, status: "error" };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        dispatch({ type: "reset" });
+        const response = await fetch("http://localhost:9000/questions");
+
+        if (!response.ok) throw new Error("Whoops something went wrong");
+
+        const data = await response.json();
+
+        dispatch({ type: "dataReceive", payload: data });
+      } catch (err) {
+        dispatch({ type: "error" });
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
